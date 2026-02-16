d34d1d12068259a15289b8a1dd31d780bc75b101
raul <ramses434@gmail.com>
2026-02-16 14:19:57 +0100

    version1

---
+++ Olimpicfantasy/app/src/main/java/com/example/olimpicfantasy/domain/model/atleta.kt
@@ -0,0 +1,24 @@
+package com.example.olimpicfantasy.domain.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Atleta(
+    val atletaID: AtletaID,
+    val nombre: String,
+
+
+    val deporteId: DeporteId,
+    val fotoUrl: String?,
+
+    val precio: Long,
+    val estado: EstadoAtleta
+) {
+
+    init {
+        require(precio >= 0) { "El precio no puede ser negativo" }
+        if (fotoUrl != null) {
+            require(fotoUrl.startsWith("http")) { "La URL de la foto debe ser válida" }
+        }
+    }
+}
---
+++ Olimpicfantasy/app/src/main/java/com/example/olimpicfantasy/domain/model/domainsIds.kt
@@ -0,0 +1,20 @@
+package com.example.olimpicfantasy.domain.model
+
+import kotlinx.serialization.Serializable
+
+@JvmInline
+@Serializable
+value class AtletaID(val value: Int)
+
+@JvmInline
+@Serializable
+value class DeporteId(val value:Int)
+
+@JvmInline
+@Serializable
+value class  UserID (val value: Int)
+
+@JvmInline
+@Serializable
+value class LigaID(val value: Int)
+
